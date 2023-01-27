2b928b0d7ed96bd63b3eff4cf4fd9342e0f1196c
Nikos Nikolaidis <nikosnikolaidis@users.noreply.github.com>
2023-01-27 18:06:35 +0000

    Fix controller

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,3 +1,8 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/api")
